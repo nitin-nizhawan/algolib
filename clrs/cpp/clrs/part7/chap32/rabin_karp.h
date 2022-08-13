c0a2571eb6dbd207a8aa21fd1b784d8054b0b386
Nitin Nizhawan <nitin.nizhawan@gmail.com>
2022-08-13 11:18:04 +0000

    Added documentaion for rabin rabin_karp

--- clrs/cpp/clrs/part7/chap32/rabin_karp.h
+++ clrs/cpp/clrs/part7/chap32/rabin_karp.h
@@ -19,7 +19,12 @@ namespace part7
             int val();
             int slide(const string &s, int i, int len);
         };
-
+        
+        /**
+         * @brief this class implements
+         * rabin karp search
+         * 
+         */
         class RabinKarpSearch
         {
 
@@ -28,7 +33,23 @@ namespace part7
             bool IsAt(const string &haystack, const string &needle, int pos) const;
 
         public:
+            /**
+             * @brief Construct a new Rabin Karp Search object
+             * Creates an instance of rabing  karp searcher
+             * for a given pattern. This instance
+             * can be used to search on any number of texts
+             * @param pattern the pattern which has to be searched
+             */
             RabinKarpSearch(const string &pattern);
+            /**
+             * @brief method to search in a text
+             * This method will search for the pattern with
+             * which this instance of searcher was created.
+             * It returns the first occurance of the instance
+             * @param haystack the text which has to be searched
+             * @return int index of the pattern. If pattern is not found
+             * then -1 is returned
+             */
             int Search(const string &haystack) const;
         };
     }
